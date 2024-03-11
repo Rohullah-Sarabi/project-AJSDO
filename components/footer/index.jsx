2b185c1df3e8e2b93bc95f0780d221ee84ffe407
Asadullah Ahmadi <asad.ahmadi1405@gmail.com>
2024-03-11 13:44:21 +0430

    I change logo of twiter to x

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { FaFacebookF, FaLinkedin, FaTwitter } from "react-icons/fa6";
+import { FaFacebookF, FaLinkedin, FaTwitter, FaX } from "react-icons/fa6";
 import { TfiYoutube } from "react-icons/tfi"
 import { SiGmail } from "react-icons/si"
 
@@ -50,7 +50,7 @@ function Footer() {
                                     <FaFacebookF className="text-2xl"/>
                                 </Link>
                                 <Link href={""}>
-                                    <FaTwitter
+                                    <FaX
                                      className="text-2xl"/>
                                 </Link>
                                 <Link href={""}>
