5e6a960ca4a8f5a6481ca496f62327e2d8bbbbc1
Asadullah Ahmadi <asad.ahmadi1405@gmail.com>
2024-03-16 11:19:11 +0430

    .

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { FaFacebookF, FaLinkedin, FaTwitter, FaX } from "react-icons/fa6";
+import { FaFacebookF, FaLinkedin,FaX } from "react-icons/fa6";
 import { TfiYoutube } from "react-icons/tfi"
 import { SiGmail } from "react-icons/si"
 
